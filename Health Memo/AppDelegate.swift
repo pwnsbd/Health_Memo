463e5bce1928c105c1d999e7ce44c062a07f70dc
pawan subedi <justpawan7@gmail.com>
2020-10-29 15:56:00 -0500

    COMMIT

--- Health Memo/AppDelegate.swift
+++ Health Memo/AppDelegate.swift
@@ -11,7 +11,7 @@ import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    //commit
+    //comMIT
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
